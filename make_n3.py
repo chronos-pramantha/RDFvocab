16c6bec2c78f5a69b08effbc24221214df10d409
Lorenzo <tunedconsulting@gmail.com>
2015-08-07 16:56:02 +0200

    use of REST content translation directly from files

--- make_n3.py
+++ make_n3.py
@@ -8,27 +8,29 @@ import os
 import pycurl
 from urllib import urlencode
 
+url = 'http://rdf-translator.appspot.com/convert/json-ld/nt/content'
 
 def _curling(url, params, file):
     """
-    GET to a remote url and print the response in a file
+    POST to a remote url and print the response in a file
     :param url: target url
     :param params: parameters in the request
-    :param file: path of the output file
+    :param file: name of the output file
     :return: body of the response
     """
     c = pycurl.Curl()
     c.setopt(c.URL, url)
-    if params: c.setopt(c.URL, url + '?' + urlencode(params))
+    # if request is GET
+    #if params: c.setopt(c.URL, url + '?' + urlencode(params))
 
     # if request is POST
-    #post_data = params
+    post_data = params
     # Form data must be provided already urlencoded.
-    #postfields = urlencode(post_data)
+    postfields = urlencode(post_data)
     # Sets request method to POST,
     # Content-Type header to application/x-www-form-urlencoded
     # and data to send in request body.
-    #c.setopt(c.POSTFIELDS, postfields)
+    c.setopt(c.POSTFIELDS, postfields)
 
     c.setopt(c.WRITEDATA, file)
     c.perform()
@@ -36,15 +38,20 @@ def _curling(url, params, file):
 
     return None
 
+
+def readjson(path):
+    with open(path, 'r') as f:
+        content = f.read()
+    return content
+
 jsonpath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'ld+json')
 ntpath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'ntriples')
 
-ld_json = (filename[:-5] for path, dirs, files in os.walk(jsonpath) for filename in files if filename.endswith(".json"))
+ld_json = ((readjson(os.path.join(path, filename)), filename[:-5]) for path, dirs, files in os.walk(jsonpath) for filename in files if filename.endswith(".json"))
 
-for f in ld_json:
+for c, f in ld_json:
+    print c
     filename = f.lower()
-    print filename
-    url = 'http://rdf-translator.appspot.com/convert/json-ld/nt/http%3A%2F%2Fontology.projectchronos.eu%2F' + filename +'%2F%3Fformat%3Djsonld'
     filename += '.ntriples'
     with open(os.path.join(ntpath, filename), 'wb+') as file:
-        _curling(url, {}, file)
+        _curling(url, {'content': c}, file)
